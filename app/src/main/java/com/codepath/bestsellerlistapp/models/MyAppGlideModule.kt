f9f82dbc4e4fbab15f86d215f74c2db9c87c8e87
sehumah <sehumah@protonmail.com>
2022-02-20 13:19:09 -0600

    added myappglidemodule to models

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/models/MyAppGlideModule.kt
@@ -0,0 +1,21 @@
+package com.codepath.bestsellerlistapp.models
+
+import android.annotation.SuppressLint
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+@GlideModule
+class MyAppGlideModule: AppGlideModule() {
+    @SuppressLint("CheckResult")
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        // Glide default Bitmap Format is set to RGB_565 since it consumed just 50% memory footprint compared to ARGB_8888.
+        // Increase memory usage for quality with:
+        builder.setDefaultRequestOptions(RequestOptions().format(DecodeFormat.PREFER_ARGB_8888))
+        super.applyOptions(context, builder)
+    }
+}
